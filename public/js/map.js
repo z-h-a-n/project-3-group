4f06afde770450dd4cc18e5aeb1cb8578f9eb7d6
zhan wang <zhanzw@gmail.com>
2015-06-22 14:33:58 +0100

    appended simple map

--- public/js/map.js
+++ public/js/map.js
@@ -1,1 +1,11 @@
 console.log('hello');
+
+
+
+
+$(document).ready(function(){
+	L.mapbox.accessToken = 'pk.eyJ1IjoiemhhbnciLCJhIjoiYzcwMjAwYzQ4MWYxZGQyMjkxMTFkYWQ0M2YxMjcwN2YifQ.q_Daj_EF2x_eGq6dl_jORw';
+
+
+var map = L.mapbox.map('map', 'mapbox.streets').setView([40, -74.50], 9);
+})
