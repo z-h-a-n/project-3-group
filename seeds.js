939ebc4bc9d0e57d637aaab83bade63b0839cb05
MacBook <MacBook@MacBooks-MacBook-Pro.local>
2015-06-22 15:25:51 +0100

    Added start and end lat/long coordinates to route model and to seeds.

--- seeds.js
+++ seeds.js
@@ -17,6 +17,10 @@ db.Comment.collection.remove();
 db.Route.create({
     start: 'LHR',
     end: 'PEK',
+    startlat: 51.507222,
+    startlong: -0.1275,
+    endlat: 39.916667, 
+    endlong: 116.383333,
     lengthDist: 2000,
     lengthTime: '4'
 
