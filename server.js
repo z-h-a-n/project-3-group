00535de658f8f5bd261f3521f48927147a67e51f
Daisy Molving <daisymolving@gmail.com>
2015-06-22 12:39:54 +0100

    linked server to ejs html index file. hello world on page

--- server.js
+++ server.js
@@ -6,3 +6,11 @@ server.listen(process.env.PORT || 3000);
 var bodyParser = require ('body-parser');
 var db = require('./models');
 
+app.set('views', './views');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req, res){
+  res.render('index');
+ });
