ed3bcd0ddb3af8d65a88fdd0f7d136b416ec4494
Daisy Molving <daisymolving@gmail.com>
2015-06-22 20:00:49 +0100

    made create function for routes, that posts to mustache template

--- public/js/app.js
+++ public/js/app.js
@@ -16,8 +16,24 @@ var Route = {
         View.render($('#route-template'), routes, $('#list-routes'));
       })
     })
+  },
+  create: function(routeParams){
+    $.post('/routes', routeParams)
+    .done(function(response){
+      var route = JSON.parse(response);
+      var template = '<li class="list-route-item">'
+      template += route.start + route.end;
+      template += 'span class="label label-default">' + route.place + '</span>';
+      template += '<button data-id="' + food.id + '" type="button" class="js-close close" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
+      template += '</li>';
+      $('#list-routes').append(template);
+    })
+    .done(function() {
+      $('#route-form').trigger('reset');
+    });
   }
 }
+
 
 var View = {
   render: function(templateElement, object, parentElement){
@@ -33,3 +49,6 @@ var View = {
     })
   }
 }
+
+
+
--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get("/routes", function (req, res){
 app.post("/routes", function (req, res){
   db.Route.create(req.body, function(err, routes){
   });
-  var newPost = req.body;
+  var newRoute = req.body;
   routes.length >= 1 ? newRoute.id = routes[routes.length -1].id +1 : newRoute.id = 0;
   routes.push(newRoute);
   res.send(JSON.stringify(newRoute));
