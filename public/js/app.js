68b004cbfa4fc9ae01bbddb0733dae40516f9b6d
MacBook <MacBook@MacBooks-MacBook-Pro.local>
2015-06-22 15:45:39 +0100

    Checked get / and changed the code slightly to be more like the eat.ly example we did in class.  There doesnt seem to be any change though and its not referening moustache now so may be a mistake...

--- public/js/app.js
+++ public/js/app.js
@@ -21,13 +21,29 @@ View = {
 Route = {
   all: function() {
     $.get('/routes', function(res) {
-      var routes = JSON.parse(res)
+      // var routes = JSON.parse(res)
+      // This line is diffent in mine, routes = res
+      var routes = res;
+
+  //     $.each(routes, function(index, route) {
+  //       var template = $('#route-template').html();
+  //       Mustache.parse(template);
+  //       var rendered = Mustache.render(template, route);
+  //       $('#list-routes').append(rendered);
+  //     })
+  //   });
+  // }
+  all: function() {
+    $.get('/routes', function(response) {
+      var routes = response;
       $.each(routes, function(index, route) {
-        var template = $('#route-template').html();
-        Mustache.parse(template);
-        var rendered = Mustache.render(template, route);
-        $('#list-routes').append(rendered);
+        View.render($('#list-routes'), route, $('#route-ul'));
       })
     });
-  }
+  },
+
+
+
+
+
 }
