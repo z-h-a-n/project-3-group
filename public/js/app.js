62f6ae8c6524125f731bf7817c7189b16a297ba9
Daisy Molving <daisymolving@gmail.com>
2015-06-23 17:47:10 +0100

    modal completed and animated

--- public/js/app.js
+++ public/js/app.js
@@ -1,54 +1,53 @@
 
 
-<<<<<<< HEAD
-  Route.all();
+
+//   Route.all();
 
 
-});
+// });
 
-var Route = {
-  all: function(){
-    $.get('/routes', function(routes){
-      $.each(routes, function(index, routes){
-        console.log('route-all');
-        View.render($('#route-template'), routes, $('#list-routes'));
-      })
-    })
-  },
-  create: function(routeParams){
-    $.post('/routes', routeParams)
-    .done(function(response){
-      var route = JSON.parse(response);
-      var template = '<li class="list-route-item">'
-      template += route.start + route.end;
-      template += 'span class="label label-default">' + route.place + '</span>';
-      template += '<button data-id="' + food.id + '" type="button" class="js-close close" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
-      template += '</li>';
-      $('#list-routes').append(template);
-    })
-    .done(function() {
-      $('#route-form').trigger('reset');
-    });
-  }
-}
-
-
-var View = {
-  render: function(templateElement, object, parentElement){
-    var template = templateElement.html();
-    Mustache.parse(template);
-    var rendered = Mustache.render(template, object);
-    parentElement.append(rendered);
-  },
-  initialize: function(){
-    $('#route-form').on('submit', function(event){
-      event.preventDefault();
-      console.log($this);
-    })
-  }
-}
+// var Route = {
+//   all: function(){
+//     $.get('/routes', function(routes){
+//       $.each(routes, function(index, routes){
+//         console.log('route-all');
+//         View.render($('#route-template'), routes, $('#list-routes'));
+//       })
+//     })
+//   },
+//   create: function(routeParams){
+//     $.post('/routes', routeParams)
+//     .done(function(response){
+//       var route = JSON.parse(response);
+//       var template = '<li class="list-route-item">'
+//       template += route.start + route.end;
+//       template += 'span class="label label-default">' + route.place + '</span>';
+//       template += '<button data-id="' + food.id + '" type="button" class="js-close close" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
+//       template += '</li>';
+//       $('#list-routes').append(template);
+//     })
+//     .done(function() {
+//       $('#route-form').trigger('reset');
+//     });
+//   }
+// }
 
 
+// var View = {
+//   render: function(templateElement, object, parentElement){
+//     var template = templateElement.html();
+//     Mustache.parse(template);
+//     var rendered = Mustache.render(template, object);
+//     parentElement.append(rendered);
+//   },
+//   initialize: function(){
+//     $('#route-form').on('submit', function(event){
+//       event.preventDefault();
+//       console.log($this);
+//     })
+//   }
+// }
 
-=======
->>>>>>> e7855e0bef668fcfb0da0251e26227bef5a6a573
+
+
+
