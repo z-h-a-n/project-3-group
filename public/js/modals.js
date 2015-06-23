efc579956d5c01397d3c9c957e9d494af90d0d52
Daisy Molving <daisymolving@gmail.com>
2015-06-23 13:34:44 +0100

    made a working modal, styled it

--- public/js/modals.js
+++ public/js/modals.js
@@ -1,49 +1,38 @@
 
 function welcomeDialog() {
- $( "#welcomeToSoar" ).dialog({
-  dialogClass: "no-close",
-  buttons: [
-    {
-      text: "OK",
-      click: function() {
-        $( this ).dialog( "close" );
+  $("#welcomeToSoar").dialog({
+    dialogClass: "no-close",
+    buttons: [
+      {
+        text: "OK",
+        click: function() {
+          $( this ).dialog("close");
+        }
       }
-    }
-  ]
-});
-  $("#welcomeToSoar").dialog({
-    height: 1000,
-    width: 300
-  });
-  $("#welcomeToSoar").dialog({
-  buttons: [
-    {
-      text: "Let's do it!",
-      "class": 'againButtonClass',
-      click: function() {
-        $( this ).dialog( "close");
-    }
-  }
-  ]
-});
-   $( "#welcomeToSoar" ).dialog({
-    modal: true
-    });
-    $( "#welcomeToSoar" ).dialog({
-  close: function(event, ui) {
+    ],
+    height: 600,
+    width: 1000,
+    buttons: [
+      {
+        text: "Let's do it!",
+        "class": 'againButtonClass',
+        click: function() {
+          $( this ).dialog( "close");
+        }
+      }
+    ],
+    modal: true,
+    close: function(event, ui) {
       window.location.reload();
-    }
+    },
+    autoOpen: true
   });
-    $("#welcomeToSoar").dialog({
-      autoOpen: true
-    });
-
 }
-
-
+ 
 
 $(document).ready(function(){
 
+  console.log("hello thus is modal");
   welcomeDialog();
 
 });
