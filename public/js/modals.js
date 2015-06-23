1001382bcbf763497de0c8395b8b6fd5d23f18f1
Daisy Molving <daisymolving@gmail.com>
2015-06-23 11:42:22 +0100

    added jQuery.ui file to scripts, styled modal

--- public/js/modals.js
+++ public/js/modals.js
@@ -1,23 +1,49 @@
+
+function welcomeDialog() {
+ $( "#welcomeToSoar" ).dialog({
+  dialogClass: "no-close",
+  buttons: [
+    {
+      text: "OK",
+      click: function() {
+        $( this ).dialog( "close" );
+      }
+    }
+  ]
+});
+  $("#welcomeToSoar").dialog({
+    height: 1000,
+    width: 300
+  });
+  $("#welcomeToSoar").dialog({
+  buttons: [
+    {
+      text: "Let's do it!",
+      "class": 'againButtonClass',
+      click: function() {
+        $( this ).dialog( "close");
+    }
+  }
+  ]
+});
+   $( "#welcomeToSoar" ).dialog({
+    modal: true
+    });
+    $( "#welcomeToSoar" ).dialog({
+  close: function(event, ui) {
+      window.location.reload();
+    }
+  });
+    $("#welcomeToSoar").dialog({
+      autoOpen: true
+    });
+
+}
+
+
+
 $(document).ready(function(){
 
   welcomeDialog();
 
 });
-
-  function welcomeDialog() {
-    $("#welcomeToSoar").dialog({
-      dialogClass: "no-close",
-      buttons: [
-        {
-          text: "Let's do it!"
-          click: function () {
-            $(this).dialog("close");
-          }
-        }]
-      height: 250;
-      modal: true;
-      close: function(event, ui) {
-      window.location.reload();
-      } 
-    })
-  }
