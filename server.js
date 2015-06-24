4af6ca47495453f043bb657e1d604cab14d065e5
MacBook <MacBook@MacBooks-MacBook-Pro.local>
2015-06-24 10:32:22 +0100

    final commit before merge

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
-//  Views / File system(?)
+//  Views / ejs Templates
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
